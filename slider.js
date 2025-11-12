cfdcebededa02e9417ec24f112c1e2b39945808c
chixchix <165348643+chixchix@users.noreply.github.com>
2025-11-12 10:57:27 -0500

    first

---
+++ slider.js
@@ -0,0 +1,41 @@
+let slideIndex=0;  //counter for the slides
+
+const showSlides = () => {
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName("dot");
+
+    //to hide all the slides first
+    for(let i=0; i < slides.length; i++){
+        slides[i].style.display ="none";
+    }
+
+    //move to the next slide
+    slideIndex++;
+
+    //If we reach the end go back to the first slide
+    if (slideIndex > slides.length){
+        slideIndex=1;
+    }
+
+    //remove "active " from all dots
+    for(let i=0; i <dots.length; i++){
+        dots[i].classList.remove("active");
+    }
+
+    //show the current slide
+    slides[slideIndex - 1].style.display = "block";
+
+    // make the matching dot active
+    dots[slideIndex - 1].classList.add("active");
+
+    // change slide every 5 seconds
+    setTimeout (showSlides, 5000);
+};
+
+showSlides(); //calls the function when the page loads
+
+// optional: if you want to click dots to jump to a slide
+  const currentSlide = (n) => {
+    slideIndex = n - 1;
+    showSlides();
+  };
